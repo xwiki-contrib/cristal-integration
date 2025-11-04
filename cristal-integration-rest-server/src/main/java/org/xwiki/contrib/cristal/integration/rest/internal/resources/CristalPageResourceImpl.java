1cf87eac3d43a65bf4f46c5cd7deaf431fc65ebf
Pierre Jeanjean <pierre.jeanjean@xwiki.com>
2025-11-04 07:00:37 -0800

    CRISTALINT-14: Revision is ignored for HTML rendering (#53)

--- cristal-integration-rest-server/src/main/java/org/xwiki/contrib/cristal/integration/rest/internal/resources/CristalPageResourceImpl.java
+++ cristal-integration-rest-server/src/main/java/org/xwiki/contrib/cristal/integration/rest/internal/resources/CristalPageResourceImpl.java
@@ -29,10 +29,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.cristal.integration.rest.resources.CristalPageResource;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.rest.XWikiResource;
 import org.xwiki.rest.XWikiRestException;
-import org.xwiki.contrib.cristal.integration.rest.resources.CristalResource;
 import org.xwiki.security.authorization.AccessDeniedException;
 import org.xwiki.security.authorization.ContextualAuthorizationManager;
 import org.xwiki.security.authorization.Right;
@@ -40,6 +40,7 @@ import org.xwiki.security.authorization.Right;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.api.Document;
+import com.xpn.xwiki.doc.DocumentRevisionProvider;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.web.ExternalServletURLFactory;
 import com.xpn.xwiki.web.XWikiURLFactory;
@@ -48,16 +49,19 @@ import jakarta.json.Json;
 import jakarta.json.JsonObjectBuilder;
 
 /**
- * Default implementation of {@link CristalResource}.
+ * Default implementation of {@link CristalPageResource}.
  *
  * @version $Id$
  */
 @Component
-@Named("org.xwiki.contrib.cristal.integration.rest.internal.resources.CristalResourceImpl")
-public class CristalResourceImpl extends XWikiResource implements CristalResource
+@Named("org.xwiki.contrib.cristal.integration.rest.internal.resources.CristalPageResourceImpl")
+public class CristalPageResourceImpl extends XWikiResource implements CristalPageResource
 {
     @Inject
     private ContextualAuthorizationManager contextualAuthorizationManager;
+
+    @Inject
+    private DocumentRevisionProvider documentRevisionProvider;
 
     @Override
     public Response getPage(String wikiName, String spaceName, String pageName, String format,
@@ -83,7 +87,14 @@ public class CristalResourceImpl extends XWikiResource implements CristalResourc
                 XWikiContext context = getXWikiContext();
                 XWikiURLFactory externalUrlFactory = new ExternalServletURLFactory(context);
                 context.setURLFactory(externalUrlFactory);
-                XWikiDocument xdoc = context.getWiki().getDocument(doc.getDocumentReference(), context);
+
+                XWikiDocument xdoc;
+                if (revision != null && !revision.isEmpty()) {
+                    xdoc = this.documentRevisionProvider.getRevision(documentReference, revision);
+                } else {
+                    xdoc = context.getWiki().getDocument(doc.getDocumentReference(), context);
+                }
+
                 context.setDoc(xdoc);
                 builder.add("html", xdoc.getRenderedContent(context));
 
